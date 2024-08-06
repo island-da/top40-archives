96882f52e1712ac1ffce19a86beded5e79a737ce
island-da <sknt0309@gmail.com>
2024-08-06 15:56:54 +0900

    スクレイピングを別モジュールに移動

---
+++ scraper/archives.go
@@ -0,0 +1,84 @@
+package scraper
+
+import (
+	"encoding/csv"
+	"fmt"
+	"html"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/gocolly/colly"
+)
+
+func Archives() {
+	c := colly.NewCollector()
+
+	c.OnHTML("div.month", func(e *colly.HTMLElement) {
+		e.ForEach("a", func(_ int, a *colly.HTMLElement) {
+			onClickAttr := a.Attr("onclick")
+
+			if strings.Contains(onClickAttr, "loadDataFile") {
+				start := strings.Index(onClickAttr, "'") + 1
+				end := strings.LastIndex(onClickAttr, "'")
+				apiURL := "https://www.tvk-yokohama.com/top40/2022/" + onClickAttr[start:end]
+
+				parsedURL, err := url.Parse(apiURL)
+				if err != nil {
+					log.Printf("Failed to parse URL %s: %v", apiURL, err)
+					return
+				}
+
+				year, date := "", ""
+				segments := strings.Split(strings.Trim(parsedURL.Path, "/"), "/")
+				if len(segments) >= 2 {
+					year = segments[len(segments)-2]
+					dateWithExt := segments[len(segments)-1]
+					date = strings.TrimSuffix(dateWithExt, path.Ext(dateWithExt))
+				} else {
+					fmt.Println("Not enough segments in path:", parsedURL.Path)
+					return
+				}
+
+				res, err := http.Get(apiURL)
+				if err != nil {
+					fmt.Println("Failed to make API request:", err)
+					return
+				}
+				defer res.Body.Close()
+				time.Sleep(1 * time.Second)
+
+				reader := csv.NewReader(res.Body)
+				for {
+					record, err := reader.Read()
+					if err == io.EOF {
+						break
+					}
+					if err != nil {
+						log.Fatalf("Failed to read CSV: %v", err)
+					}
+
+					joinedRecord := strings.Join(record, ",")
+					fields := strings.Split(joinedRecord, ",")
+					if len(fields) >= 2 {
+						title := html.UnescapeString(fields[len(fields)-2])
+						artistName := html.UnescapeString(fields[len(fields)-1])
+						fmt.Printf("%s_%s %s / %s\n", year, date, artistName, title)
+					} else {
+						fmt.Println("Not enough fields")
+					}
+				}
+			}
+		})
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("https://www.tvk-yokohama.com/top40/2022/archives.html")
+}
